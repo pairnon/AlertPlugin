677774622313f34764f4747d338604e5bfc9c2aa
pairnon <janpairnon@gmail.com>
2024-06-09 18:56:44 -0700

    Refactor / fix bug

--- app/src/main/java/io/papermc/alertplugin/commands/CommandBeginRestartAlerts.java
+++ app/src/main/java/io/papermc/alertplugin/commands/CommandBeginRestartAlerts.java
@@ -12,6 +12,8 @@ import io.papermc.alertplugin.Broadcasting;
 public class CommandBeginRestartAlerts implements CommandExecutor {
 
     private static final int DURATION_IN_MINUTES = 10;
+
+    private static int schedulingValueContainer = 0;
     
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -33,126 +35,37 @@ public class CommandBeginRestartAlerts implements CommandExecutor {
         Broadcasting.broadcastRedAlert("Server restarting in " + DURATION_IN_MINUTES + " minutes.");
         Timer timer = new Timer();
 
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                Broadcasting.broadcastRedAlert("Server restarting in 5 minutes.");
-            }
-        }, calculateDelayFromMinutes(5));
+        for (int i = 5; i >= 1; i--) {
+            schedulingValueContainer = i;
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    Broadcasting.broadcastRedAlert("Server restarting in " + schedulingValueContainer + " minutes.");
+                }
+            }, calculateDelayFromMinutes(schedulingValueContainer));
 
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                Broadcasting.broadcastRedAlert("Server restarting in 4 minutes.");
-            }
-        }, calculateDelayFromMinutes(4));
+        }
 
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                Broadcasting.broadcastRedAlert("Server restarting in 3 minutes.");
-            }
-        }, calculateDelayFromMinutes(3));
-        
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                Broadcasting.broadcastRedAlert("Server restarting in 2 minutes.");
-            }
-        }, calculateDelayFromMinutes(2));
-        
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                Broadcasting.broadcastRedAlert("Server restarting in 1 minute.");
-            }
-        }, calculateDelayFromMinutes(2));
-        
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                Broadcasting.broadcastRedAlert("Server restarting in 30 seconds.");
-            }
-        }, calculateDelayFromSeconds(30));
+        for (int i = 30; i >= 20; i -= 10) {
+            schedulingValueContainer = i;
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    Broadcasting.broadcastRedAlert("Server restarting in " + schedulingValueContainer + " seconds.");
+                }
+            }, calculateDelayFromSeconds(schedulingValueContainer));
+        }
 
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                Broadcasting.broadcastRedAlert("Server restarting in 20 seconds.");
-            }
-        }, calculateDelayFromSeconds(20));
+        for (int i = 10; i >= 1; i--) {
+            schedulingValueContainer = i;
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    Broadcasting.broadcastRedAlert("Server restarting in " + schedulingValueContainer + " seconds.");
+                }
+            }, calculateDelayFromSeconds(schedulingValueContainer));
 
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                Broadcasting.broadcastRedAlert("Server restarting in 10 seconds.");
-            }
-        }, calculateDelayFromSeconds(10));
-
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                Broadcasting.broadcastRedAlert("Server restarting in 9 seconds.");
-            }
-        }, calculateDelayFromSeconds(9));
-
-        
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                Broadcasting.broadcastRedAlert("Server restarting in 8 seconds.");
-            }
-        }, calculateDelayFromSeconds(8));
-        
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                Broadcasting.broadcastRedAlert("Server restarting in 7 seconds.");
-            }
-        }, calculateDelayFromSeconds(7));
-        
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                Broadcasting.broadcastRedAlert("Server restarting in 6 seconds.");
-            }
-        }, calculateDelayFromSeconds(6));
-        
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                Broadcasting.broadcastRedAlert("Server restarting in 5 seconds.");
-            }
-        }, calculateDelayFromSeconds(5));
-        
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                Broadcasting.broadcastRedAlert("Server restarting in 4 seconds.");
-            }
-        }, calculateDelayFromSeconds(4));
-        
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                Broadcasting.broadcastRedAlert("Server restarting in 3 seconds.");
-            }
-        }, calculateDelayFromSeconds(3));
-        
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                Broadcasting.broadcastRedAlert("Server restarting in 2 seconds.");
-            }
-        }, calculateDelayFromSeconds(2));
-
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                Broadcasting.broadcastRedAlert("Server restarting in 1 second.");
-            }
-        }, calculateDelayFromSeconds(1));
-
+        }
     }
 
     private static int calculateDelayFromMinutes(int remainingMinutes) {
@@ -160,7 +73,7 @@ public class CommandBeginRestartAlerts implements CommandExecutor {
     }
 
     private static int calculateDelayFromSeconds(int remainingSeconds) {
-        return ((DURATION_IN_MINUTES * 60) - remainingSeconds) * 60000;
+        return ((DURATION_IN_MINUTES * 60) - remainingSeconds) * 1000;
     }
 
 }
