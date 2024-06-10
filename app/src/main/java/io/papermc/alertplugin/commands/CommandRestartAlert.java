bbd1ce1ad6f220614f0617c43e9ea00aae233c1f
pairnon <janpairnon@gmail.com>
2024-06-09 18:09:30 -0700

    Update response messages

--- app/src/main/java/io/papermc/alertplugin/commands/CommandRestartAlert.java
+++ app/src/main/java/io/papermc/alertplugin/commands/CommandRestartAlert.java
@@ -4,12 +4,19 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+import io.papermc.alertplugin.Broadcasting;
+
 public class CommandRestartAlert implements CommandExecutor {
     
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        sender.sendMessage("Beginning restart alerts.");
+        if (!sender.hasPermission("minecraft.op")) {
+            Broadcasting.sendErrorResponse(sender, "You do not have access to this command.");
+            return true;
+        }
+        
+        Broadcasting.sendSuccessResponse(sender, "Beginning restart alerts.");
 
         return true;
     }
