9a63945c0c9bf5ec1b114719eb5088a020be3a07
pairnon <janpairnon@gmail.com>
2024-06-09 17:24:43 -0700

    Add run-paper

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,6 +9,8 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
+
+    id("xyz.jpenilla.run-paper") version "2.2.0"
 }
 
 repositories {
