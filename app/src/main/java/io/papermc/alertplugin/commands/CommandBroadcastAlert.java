f641813cd1747abf6cca96605f0c48cb22e98906
pairnon <janpairnon@gmail.com>
2024-06-13 22:28:30 -0700

    Implement broadcastalert command

--- app/src/main/java/io/papermc/alertplugin/commands/CommandBroadcastAlert.java
+++ app/src/main/java/io/papermc/alertplugin/commands/CommandBroadcastAlert.java
@@ -11,13 +11,24 @@ public class CommandBroadcastAlert implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-
         if (!sender.hasPermission("minecraft.op")) {
             Broadcasting.sendErrorResponse(sender, "You do not have access to this command.");
             return true;
         }
 
-        sender.sendMessage("broadcastalert");
+        if (args.length == 0) {
+            Broadcasting.sendErrorResponse(sender, "You must specify a message.");
+            return true;
+        }
+
+        String message = "";
+
+        for (String word : args) {
+            message += word;
+            message += " ";
+        }
+
+        Broadcasting.broadcastRedAlert(message);
 
         return true;
     }
