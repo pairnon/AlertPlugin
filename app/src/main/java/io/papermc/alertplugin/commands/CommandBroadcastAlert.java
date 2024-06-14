151a98bf1006893c996ad03a061487b78aad6785
pairnon <janpairnon@gmail.com>
2024-06-13 22:23:41 -0700

    Add op requirement

--- app/src/main/java/io/papermc/alertplugin/commands/CommandBroadcastAlert.java
+++ app/src/main/java/io/papermc/alertplugin/commands/CommandBroadcastAlert.java
@@ -4,11 +4,18 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+import io.papermc.alertplugin.Broadcasting;
+
 public class CommandBroadcastAlert implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
+
+        if (!sender.hasPermission("minecraft.op")) {
+            Broadcasting.sendErrorResponse(sender, "You do not have access to this command.");
+            return true;
+        }
 
         sender.sendMessage("broadcastalert");
 
